97ffd2feaea58b62a1d223be059be5307d4aaf15
gilbertben9 <bjg4uc@virginia.edu>
2015-03-30 16:24:46 -0400

    yeah

--- SecureWitness/SecureWitness/views.py
+++ SecureWitness/SecureWitness/views.py
@@ -9,5 +9,5 @@ def admin(request):
     return render(request, 'AdminHomePage.html')
 def reader(request):
     return render(request, 'ReaderHomepage.html')
-def index(request):
-    return render(request, 'login.html')
+#def index(request):
+#    return render(request, 'login.html')
