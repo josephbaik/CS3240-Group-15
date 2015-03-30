ebeb34464b8e3bc04b968608fd1466695c44f617
gilbertben9 <bjg4uc@virginia.edu>
2015-03-30 16:19:24 -0400

    url changes

--- SecureWitness/SecureWitness/urls.py
+++ SecureWitness/SecureWitness/urls.py
@@ -6,6 +6,10 @@ urlpatterns = patterns('',
     url(r'^$', 'SecureWitness.views.index', name='Home'),
     # url(r'^blog/', include('blog.urls')),
 
+    
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+    'template_name': 'login.html'
+    }),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^reporterhome', 'SecureWitness.views.reporter', name='Home'),
     url(r'^adminhome', 'SecureWitness.views.admin', name='Home'),
