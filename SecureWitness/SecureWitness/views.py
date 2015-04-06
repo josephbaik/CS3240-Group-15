ca64e473c28af692262e041b7ff224207e7f6e81
Joseph Baik <josephsbaik@gmail.com>
2015-04-06 16:01:48 -0400

    fileupload

--- SecureWitness/SecureWitness/views.py
+++ SecureWitness/SecureWitness/views.py
@@ -38,24 +38,3 @@ def reader(request):
 def login(request):
     return render(request, 'login.html')
 
-def handle_uploads(request, keys):
-    saved = []
-
-    upload_dir = date.today().strftime(settings.UPLOAD_PATH)
-    upload_full_path = os.path.join(settings.MEDIA_ROOT, upload_dir)
-
-    if not os.path.exists(upload_full_path):
-        os.makedirs(upload_full_path)
-
-    for key in keys:
-        if key in request.FILES:
-            upload = request.FILES[key]
-            while os.path.exists(os.path.join(upload_full_path, upload.name)):
-                upload.name = '_' + upload.name
-            dest = open(os.path.join(upload_full_path, upload.name), 'wb')
-            for chunk in upload.chunks():
-                dest.write(chunk)
-            dest.close()
-            saved.append((key, os.path.join(upload_dir, upload.name)))
-    # returns [(key1, path1), (key2, path2), ...]
-    return saved
