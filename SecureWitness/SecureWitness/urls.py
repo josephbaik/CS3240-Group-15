32e2a46919f994e439afb95284fc4e00de45be48
Joseph Baik <josephsbaik@gmail.com>
2015-04-06 16:28:40 -0400

    url merge

--- SecureWitness/SecureWitness/urls.py
+++ SecureWitness/SecureWitness/urls.py
@@ -13,16 +13,12 @@ urlpatterns = patterns('',
     url(r'^reporterhome', 'SecureWitness.views.reporter', name='Home'),
     url(r'^adminhome', 'SecureWitness.views.adm', name='Home'),
     url(r'^readerhome', 'SecureWitness.views.reader', name='Home'),
-<<<<<<< HEAD
     url(r'^$', login.views.ListUserView.as_view(), name='user-list'),
     url(r'^new$', login.views.CreateUserView.as_view(), name='user-new',),
     url(r'^edit/(?P<pk>\d+)/$', login.views.UpdateUserView.as_view(), name='user-edit',),
     url(r'^delete/(?P<pk>\d+)/$', login.views.DeleteUserView.as_view(), name='user-delete',),
     url(r'^(?P<pk>\d+)/$', login.views.UserView.as_view(), name='user-view',),
-   )
 
 
-=======
-    url(r'^loginpage', 'SecureWitness.views.login', name='Login' )
+
 )
->>>>>>> ca64e473c28af692262e041b7ff224207e7f6e81
