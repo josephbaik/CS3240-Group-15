bbdcac2a5979cd984d0a73ccdc8d42f511d11684
Joseph Baik <josephsbaik@gmail.com>
2015-04-19 22:47:33 -0400

    report admin

--- SecureWitness/reportUpload/admin.py
+++ SecureWitness/reportUpload/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Report
 # Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Report
+
+admin.site.register(Report, ReportAdmin)
